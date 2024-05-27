e002189feb7bf56b2a0971ede8d225297fe8dec3
BluuPenguine <kondwanisich1@gmail.com>
2024-05-27 16:06:43 +0300

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,72 @@
+
+//Write a C++ program (name it final Exam) that you hist un Your Organization within Github
+// Manipulate a matrix of doubles, with dimensions 4 x £, which is declared in the main(i.e. double matrix[4][3])
+// 
+//Kondwani David Sichinga
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//the double matrix is initalized with the values of 4 and 3 in the arrays
+	double matrix[4][3];
+	int choice = 0;
+
+
+	while (choice != 4)
+	{
+
+		//The user is prompted to input a choice from a menu ranging from 1 - 4
+		//It is done by using a loop that will reitterate until the value of 4 is selected
+
+		cout << "1) Enter data in matrix (range -2.5 to 2.5)" << endl;
+		cout << "2) Sum of all numbers" << endl;
+		cout << "3) Show data int matrix" << endl;
+		cout << "4) Exit" << endl;
+		cout << "Please enter choice: " ;
+		cin >> choice;
+
+
+
+			switch (choice)
+			{
+				case 1:
+				{
+
+					break;
+				}
+
+				case 2:
+				{
+
+					break;
+				}
+
+				case 3:
+				{
+
+					break;
+				}
+
+				case 4:
+				{
+
+					break;
+				}
+
+				default:
+				{
+
+					break;
+				}
+
+
+			}
+
+}
+
+
+	return 0;
+}
