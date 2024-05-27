7e813b7b1870183940b36bd3971e475aa1b84ad4
BluuPenguine <kondwanisich1@gmail.com>
2024-05-27 17:42:13 +0300

    the main as well as the functions all work correctly within the source cpp

--- FinalExam.cpp
+++ FinalExam.cpp
@@ -1,60 +1,6 @@
+
 #include <iostream>
 #include <cassert>
+#include "FinalExam.h"
 using namespace std;
 
-void EnterMatrixData(double matrix[4][3], const int SIZE1, const int SIZE2);
-void SumMatrixData(double matrix[4][3], const int SIZE1, const int SIZE2);
-void ShowMatrixData(double matrix[4][3], const int SIZE1, const int SIZE2);
-
-
-
-
-//Here the user is prompted to input data that is between -2.5 and 2.5 using 2 for loops
-//the data is validated using a while loop to ensure the data entred is between -2.5 and 2.5
-void EnterMatrixData(double matrix[4][3], const int SIZE1, const int SIZE2)
-{
-	for (int i = 0; i < SIZE1; i++)
-	{
-		for (int j = 0; j < SIZE2; j++)
-		{
-			cin >> matrix[i][j];
-		}
-
-		cout << endl;
-	}
-	;
-}
-
-void SumMatrixData(double matrix[4][3], const int SIZE1, const int SIZE2)
-{
-	double sum = 0;
-
-	for (int i = 0; i < SIZE1; i++)
-	{
-		for (int j = 0; j < SIZE2; j++)
-		{
-			sum +=  matrix[i][j];
-		}
-
-		cout << endl;
-	}
-	;
-}
-
-
-//Here the function for Show Matrix data is implemented using a for loop within a for loop
-//the final matrix is printed for the user to see
-
-void ShowMatrixData(double matrix[4][3], const int SIZE1, const int SIZE2)
-{
-	for (int i = 0; i < SIZE1; i++)
-	{
-		for (int j = 0; j < SIZE2; j++)
-		{
-			cout << matrix[SIZE1][SIZE2] << " ";
-		}
-
-		cout << endl;
-	}
-	;
-}
--- Source.cpp
+++ Source.cpp
@@ -9,12 +9,69 @@
 #include "FinalExam.cpp"
 using namespace std;
 
+//Here the user is prompted to input data that is between -2.5 and 2.5 using 2 for loops
+//the data is validated using a while loop to ensure the data entred is between -2.5 and 2.5
+void EnterMatrixData(double matrix[][3], const int SIZE1)
+{
+		double num1 = 3;
+		cout << "Enter a number between -2.5 and 2.5 :" << endl;
+
+		for (int i = 0; i < SIZE1; ++i) 
+		{
+			cout << "Row " << (i + 1) << " :";
+			for (int j = 0; j < 3; ++j)
+			{
+				cin >> matrix[i][j];
+				cout << " ";
+
+			}
+			cout << endl;
+		}
+
+		cout << endl;
+	;
+}
+
+//This function uses a for loop within a for loop to calculate the total sum of all the numbers in the matrix
+//the final value is then returned and printed for the user in the main
+double SumMatrixData(double matrix[][3], const int SIZE1)
+{
+	double sum = 0;
+
+	for (int i = 0; i < SIZE1; i++)
+		{
+		for (int j = 0; j < 3; j++)
+			sum += matrix[i][j];
+		}
+
+	return sum;
+
+}
+
+//Here a for loop within a for loop is used to print out each individual number inputted
+//as well as the row it is in
+void ShowMatrixData(double matrix[][3], const int SIZE1)
+{
+
+	cout << "Content of matrix";
+
+	for (int i = 0; i < SIZE1; i++)
+	{
+		cout << endl << "Row " << (i + 1) << ": ";
+		for (int j = 0; j < 3; j++)
+			cout << matrix[i][j] << "\t";
+	}
+	cout << endl;
+
+}
+
 
 int main()
 {
-	//the double matrix is initalized with the values of 4 and 3 in the arrays
+	//the double matrix is initalized with the constant variables of 4 and 3 in the arrays
 	const int SIZE1 = 4, SIZE2 = 3;
-	double matrix[SIZE1][SIZE2] = { 1,1,1,1 };
+	double matrix[SIZE1][SIZE2];
+	double* Sumofmatrix = 0;
 	int choice = 0;
 
 
@@ -35,19 +92,19 @@ int main()
 			{
 				case 1:
 				{
-
+					EnterMatrixData(matrix, SIZE1 );
 					break;
 				}
 
 				case 2:
 				{
-
+					cout << "The sum of the matrix is " << SumMatrixData(matrix, SIZE1) << endl << endl;					
 					break;
 				}
 
 				case 3:
 				{
-
+					ShowMatrixData(matrix, SIZE1);
 					break;
 				}
 
@@ -59,7 +116,7 @@ int main()
 
 				default:
 				{
-
+					cout << "Invalid input, Please reenter choice " << endl;
 					break;
 				}
 
